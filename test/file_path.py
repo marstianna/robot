81fa59f7cf82905946e57e19716ad0caf367ad6a
涅凤 <zoupeng.zp@alibaba-inc.com>
2023-09-11 11:24:17 +0800

    d

--- test/file_path.py
+++ test/file_path.py
@@ -1,6 +1,6 @@
 import os
 
 if __name__ == "__main__":
-    join = os.path.join(os.path.dirname(os.path.dirname(__file__)), "knowledge_base")
+    join = os.path.join(os.path.join("/home/admin/model/downloads/chatGLM2-6", ""), "tokenizer_config.json")
     print(join)
     print(os.listdir(join))
