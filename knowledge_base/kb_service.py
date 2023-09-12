3a2f25f3f788aaca002b355b66ba82509b68072d
涅凤 <zoupeng.zp@alibaba-inc.com>
2023-09-12 10:45:35 +0800

    ad

--- knowledge_base/kb_service.py
+++ knowledge_base/kb_service.py
@@ -8,7 +8,8 @@ def search_kb(query: str,top_k: int,score_threshold: float,refactor: bool = Fals
                                                       top_k=top_k, score_threshold=score_threshold)
     print(docs)
     if refactor:
-        context = "。".join([doc.page_content for doc in docs])
+        context = "\n".join([doc.page_content for doc in docs])
+        context = "针对以下语句，把每一行作为单独语句，然后根据语意将文字内容进行重排序，并润色，但是不能修改其原有内容，也不能新加内容：\n" + context
         print(llm_utils.call(context))
 
     return docs
